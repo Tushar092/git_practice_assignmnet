cfd1be6e93da9948212980afa8a516762863ad77
Tushar Singh <karansinghrajora1996@gmail.com>
2022-11-02 13:14:38 +0530

    Prime Problem code fixed by m1

--- PrimeProblem.js
+++ PrimeProblem.js
@@ -1,15 +1,23 @@
-let flag = false;
-let num = 13;
 
-for(let i = 0; i <= n; i++){
-	if(num % i == 0){
-		flag = true;
-	}else{
-		falg = false;
+function checkPrime(number){
+let count = 0;
+for(let i = 1; i <= number; i++){
+	if(number % i == 0){
+		count++;
 	}
+	
+	if(count == 2){
+		return true;
+	}else{
+		return false;
+	}
+}
+}
 
-if(flag == true){
-	console.log("Prime");
-	}else{
-	console.log("Not Prime");
-	}
+let answer = checkPrime(13);
+
+if(answer == true){
+	console.log("Prime Number");
+}else{
+	console.log("Not Prime Number");
+}
